80868936b4fdea2e37174a0b27ba1ce6b12ee3c7
Pakkerman <t0297639@hotmail.com>
2023-01-23 13:25:06 +0800

    init

---
+++ rename.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+### This script is aimed to make rename file in a single click
+### Script will take a directory and name all files inside in order from 0000 to 9999
+
+### run "chmod +x rename.sh" first in bash, to give permission to run script
+echo "hello"
